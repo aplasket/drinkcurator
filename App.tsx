b8378a3404dca55ed7abae4c73dffb31cfcccaec
Ashley Plasket <ashley.plasket@gmail.com>
2024-01-17 15:13:23 -0500

    add images

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import CustomInput from './src/components/CustomInput';
 import { CustomButton } from "./src/components/CustomButton";
+
+// Main Tabs bottom navigation
+
+
+// --- Onboarding screens ---
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <CustomInput label={"Email"} />
-      <CustomInput label={"Password"} isPassword={true}/>
-      <CustomButton label={"login"}/>
+      {/* sign in component*/}
+      <View style={styles.imageContainer}>
+        <Image source={require('./assets/drinksanimated.gif')} style={styles.image}/>
+        <Text style={styles.title}>Drink Curator</Text>
+      </View>
+      {/* image container */}
+      <View style={styles.loginContainer}>
+        <CustomInput label={"Email"} />
+        <CustomInput label={"Password"} isPassword={true}/>
+        <CustomButton label={"login"}/>
+        <Text>Don't Have an Account?</Text>
+        <TouchableOpacity>
+          <Text>SignUp</Text>
+        </TouchableOpacity>
+      </View>
+      {/* stack navigation <Sign Up /> */}
+      {/* Tab Navigation <Main /> */}
+
     </SafeAreaView>
   );
 }
@@ -16,6 +36,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
+  },
+  imageContainer: {
+    paddingTop: 16,
+    paddingHorizontal: 35,
+    alignItems: 'center',
+  },
+  image: {
+    width: "100%",
+    height: "60%",
+  },
+  title:{
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  loginContainer: {
+
+  },
+  createAccountContainer: {
+
   },
 });
