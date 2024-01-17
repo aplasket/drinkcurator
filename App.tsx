f0330b9faed8278132d15ec2d3d72e86d6522e5a
Ashley Plasket <ashley.plasket@gmail.com>
2024-01-17 11:46:37 -0500

    add first component CustomInput

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import CustomInput from './src/components/CustomInput';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <CustomInput />
+    </SafeAreaView>
   );
 }
 
