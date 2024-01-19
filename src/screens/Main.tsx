f8a3df68349f44d2f31b17ad33cd2608a675d8e1
Ashley Plasket <ashley.plasket@gmail.com>
2024-01-19 14:40:53 -0500

    add tab navigation

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,9 +1,17 @@
 import { SafeAreaView, Text } from "react-native"
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Profile } from "./Profile";
+import { Login } from "./Login";
+
+
+const Tab = createBottomTabNavigator();
 
 export const Main = () => {
   return (
-    <SafeAreaView>
-      <Text>Main</Text>
-    </SafeAreaView>
-  )
+    <Tab.Navigator>
+      {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Login" component={Login} />
+    </Tab.Navigator>
+  );
 }
