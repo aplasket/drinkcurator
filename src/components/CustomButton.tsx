7de408c86d7bcb963905a9ef72185f94537f44f2
Ashley Plasket <ashley.plasket@gmail.com>
2024-01-19 15:54:50 -0500

    fix route

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,14 +1,15 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import { ButtonProps } from "../types/types";
 import { styles } from "../styles/styles";
+import { RootStackParamList } from "../types/types";
 
 export const CustomButton = ({ label, navigation, route }: ButtonProps ) => {
-  console.log(route);
-  console.log(navigation);
+  const destinationRoute: keyof RootStackParamList = route as keyof RootStackParamList || "Main";
+
   return (
     <View style={styles.buttonContainer}>
     <TouchableOpacity style={styles.button} >
-      <Text style={styles.buttonText} onPress={() => {navigation.navigate("Main")}}>{label}</Text>
+      <Text style={styles.buttonText} onPress={() => {navigation.navigate(destinationRoute)}}>{label}</Text>
     </TouchableOpacity>
 
   </View>
