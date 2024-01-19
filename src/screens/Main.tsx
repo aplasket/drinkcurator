1d4567e41636998f5e36734e8f00499e28f40066
Ashley Plasket <ashley.plasket@gmail.com>
2024-01-19 15:55:26 -0500

    update main page to be tab navigation

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,16 +1,14 @@
 import { SafeAreaView, Text } from "react-native"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Profile } from "./Profile";
+import { Home } from "./Home";
 import { Login } from "./Login";
-
 
 const Tab = createBottomTabNavigator();
 
 export const Main = () => {
   return (
-    <Tab.Navigator>
-      {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Login" component={Login} />
     </Tab.Navigator>
   );
