a3fb454330c8997f6e8acb359f30fbea61806830
Ashley Plasket <ashley.plasket@gmail.com>
2024-01-19 13:36:41 -0500

    update custombutton with onpress navigation

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -2,16 +2,25 @@ import { Text, TouchableOpacity, View } from "react-native";
 import { ButtonProps } from "../types/types";
 import { styles } from "../styles/styles";
 
-export const CustomButton = ({label}: ButtonProps) => {
-  const handleSubmit = () => {
-    //check authentication for email and password
-  };
-
+export const CustomButton = ({ label, navigation, route }: ButtonProps ) => {
+  console.log(route);
+  console.log(navigation);
   return (
     <View style={styles.buttonContainer}>
-    <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-      <Text style={styles.buttonText}>{label}</Text>
+    <TouchableOpacity style={styles.button} >
+      <Text style={styles.buttonText} onPress={() => {navigation.navigate("Main")}}>{label}</Text>
     </TouchableOpacity>
+
   </View>
   )
 };
+
+/*
+ * const handleSubmit = () => {
+    //check authentication for email and password
+    //if email and password
+    navigation.navigate("Main")
+    // else error message of email and password
+  };
+ *
+ */
